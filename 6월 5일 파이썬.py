0d3bb11be9969127105852a7ae0f47e10f9f778f
cauwang <kkjwsm9785@gmail.com>
2024-06-13 20:23:04 +0900

    distance기능추가

--- 6월 5일 파이썬.py
+++ 6월 5일 파이썬.py
@@ -1,5 +1,6 @@
 #20191950 왕성민
-print("마지막으로 연동되는지 확인")
+import math
+
 class Vector2D:
     def __init__(self,x,y):
         self.x=x
@@ -19,10 +20,12 @@ class Vector2D:
         return self.x==other.x and self.y==other.y 
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
+    def distance(self, other):
+        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
     
     
-v1 = Vector2D(10, 20)
-v2 = Vector2D(10, 2)
+v1 = Vector2D(1, 1)
+v2 = Vector2D(1, 9)
 v3 = v1 + v2
 print(v1,'+',v2,'=', v3)
 
@@ -39,5 +42,10 @@ v3 = -v1
 print('-',v1,'=',v3)
 
 v3 = v1 == v2
+
+dist = v1.distance(v2)
+
+
+print('Distance between', v1, 'and', v2, '=', dist)
 print(v1,'==',v2,'=',v3)
 print("20191950 왕성민")
